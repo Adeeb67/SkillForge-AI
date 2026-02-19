a9ba32661b3b6a8a1d1d0810ead1de43abce43a7
Mohammed Adeeb <mohammedadeeb532@gmail.com>
2026-02-19 22:43:11 +0530

    Add learning stats fields to User model

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,9 +1,16 @@
 from sqlalchemy import Column, Integer, String
 from .database import Base
 
+
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
+
+    # Learning stats
+    xp = Column(Integer, default=0)
+    level = Column(Integer, default=1)
+    lessons_completed = Column(Integer, default=0)
+    streak = Column(Integer, default=0)
