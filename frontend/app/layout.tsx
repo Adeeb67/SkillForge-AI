6cf529ee2c697fe78e05664cd6502b6e47ec0c4e
Mohammed Adeeb <mohammedadeeb532@gmail.com>
2026-02-20 17:36:00 +0530

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,36 +1,15 @@
-"use client";
-
 import "./globals.css";
-import { usePathname, useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+
+export const metadata = {
+  title: "SkillForge AI",
+  description: "Adaptive AI Learning Platform",
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-  const router = useRouter();
-  const [mounted, setMounted] = useState(false);
-
-  // wait until browser loads
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  useEffect(() => {
-    if (!mounted) return;
-
-    const token = localStorage.getItem("token");
-
-    if (pathname.startsWith("/dashboard") && !token) {
-      router.push("/login");
-    }
-  }, [mounted, pathname]);
-
-  // prevent SSR crash
-  if (!mounted) return null;
-
   return (
     <html lang="en">
       <body>{children}</body>
