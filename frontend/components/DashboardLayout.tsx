7ab4ae6e18614857fab468e723355686aac57c16
Mohammed Adeeb <mohammedadeeb532@gmail.com>
2026-02-20 10:35:00 +0530

    Update DashboardLayout.tsx

--- frontend/components/DashboardLayout.tsx
+++ frontend/components/DashboardLayout.tsx
@@ -1,23 +1,10 @@
-"use client";
-
 import Sidebar from "./Sidebar";
 
-export default function DashboardLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function DashboardLayout({ children }: any) {
   return (
     <div className="bg-[#F6F7FB] min-h-screen">
-
-      {/* SIDEBAR */}
       <Sidebar />
-
-      {/* MAIN CONTENT AREA */}
-      <main className="ml-64 p-10">
-        {children}
-      </main>
-
+      <main className="ml-64 p-10">{children}</main>
     </div>
   );
 }
