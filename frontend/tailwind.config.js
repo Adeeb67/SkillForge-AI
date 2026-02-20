e09da28ffb969db3d10f9db8b76166c4d5028963
Mohammed Adeeb <mohammedadeeb532@gmail.com>
2026-02-20 10:12:18 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,27 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)"],
+      },
+
+      colors: {
+        primary: "#6366F1",
+        sidebar: "#F6F7FB",
+        card: "#FFFFFF",
+        textMain: "#111827",
+        textSoft: "#6B7280",
+      },
+
+      boxShadow: {
+        card: "0 2px 8px rgba(0,0,0,0.05)",
+      },
+
+      borderRadius: {
+        xl: "14px",
+      },
+    },
   },
   plugins: [],
-}
+};
