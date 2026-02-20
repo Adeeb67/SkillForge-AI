5d4cda043dd4ac353d80334dbe52e993bbec7349
Mohammed Adeeb <mohammedadeeb532@gmail.com>
2026-02-20 17:23:05 +0530

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,17 +1,39 @@
 "use client";
 
 import "./globals.css";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+  const router = useRouter();
+  const [mounted, setMounted] = useState(false);
+
+  // wait until browser loads
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (!mounted) return;
+
+    const token = localStorage.getItem("token");
+
+    if (pathname.startsWith("/dashboard") && !token) {
+      router.push("/login");
+    }
+  }, [mounted, pathname]);
+
+  // prevent SSR crash
+  if (!mounted) return null;
+
   return (
     <html lang="en">
-      <body className="bg-[#F6F7FB] text-gray-900">
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
