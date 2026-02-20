23ffc6f8ac39c2bb0464687ec3b0c715c4c4ac4f
Mohammed Adeeb <mohammedadeeb532@gmail.com>
2026-02-20 10:22:18 +0530

    Update DashboardLayout.tsx

--- frontend/components/DashboardLayout.tsx
+++ frontend/components/DashboardLayout.tsx
@@ -1,14 +1,11 @@
-import Sidebar from "./Sidebar";
-
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex bg-[#eef0f6] min-h-screen">
-      <Sidebar />
-      <main className="flex-1 p-10">{children}</main>
+    <div className="flex">
+      {children}
     </div>
   );
 }
